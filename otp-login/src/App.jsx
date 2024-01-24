23c6979c0ff8d809c86d7de00256c7e881e004b5
heyvipul <vipulgirhestar@gmail.com>
2024-01-24 15:32:33 +0530

    done

--- otp-login/src/App.jsx
+++ otp-login/src/App.jsx
@@ -3,12 +3,10 @@ import PhoneOptForm from './components/phone-login'
 
 function App() {
   
-
   return (
     <div className='App'>
       <h2>Login with phone</h2>
       <PhoneOptForm/>
-
     </div>
   )
 }
