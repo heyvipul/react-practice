79ebb20d2f9504f2c58ce87f1cef04a915b0f318
heyvipul <vipulgirhestar@gmail.com>
2024-02-21 16:06:12 +0530

    timer

--- swiggy-practice/src/components/Timer.jsx
+++ swiggy-practice/src/components/Timer.jsx
@@ -1,38 +1,36 @@
 import  { useState, useRef } from "react";
 
 const Timer = () => {
-    const [count, setCount] = useState(0);
-    const [timerRunning, setTimerRunning] = useState(false);
+    const[count,setCount] = useState(0);
+    const[timerRunning,setTimerRunning] = useState(false)
     const timerRef = useRef();
-    console.log(count);
 
-    const startTimer = () => {
-        setTimerRunning(true);
+    const startTimer = () =>{
         timerRef.current = setInterval(() => {
-            setCount((prevCount) => prevCount + 1);
+            setCount(prev => prev +1)
+            setTimerRunning(true)
         }, 1000);
-    };
-
-    const stopTimer = () => {
-        setTimerRunning(false);
+    }
+    console.log(count);
+    
+    const stopTimer = () =>{
         clearInterval(timerRef.current);
-    };
-
-    const reset = () => {
-        setCount(0);
-        clearInterval(timerRef.current);
-        setTimerRunning(false);
-    };
+        setTimerRunning(false)
+    }
+    
+    const clearTimer = () =>{
+        setCount(0)
+        clearInterval(timerRef.current)
+        setTimerRunning(false)
+    }
+    
 
     return (
         <div>
             <h2>Count : {count}</h2>
-            {!timerRunning ? (
-                <button onClick={startTimer}>Start</button>
-            ) : (
-                <button onClick={stopTimer}>Stop</button>
-            )}
-            <button onClick={reset}>Reset</button>
+            <button disabled={timerRunning} onClick={startTimer}>Start</button>
+            <button disabled={!timerRunning} onClick={stopTimer}>Stop</button>
+            <button disabled={count==0} onClick={clearTimer}>Reset</button> 
         </div>
     );
 };
